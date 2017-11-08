0a886909167af87f3cb2b50e347b468352971d79
Angelo Cigna <acigna1982@gmail.com>
2017-11-08 01:27:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution in this file!
-function calculateVertical(start, end)
+function calculateVertical(start, end){
+  let start = 1;
+  let end = 2;
+
+  //
+  let blocksTraveled = (end - start)
+
+  return blocksTraveled * feetInBlock
+
+  
+}
